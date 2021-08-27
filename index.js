3c68799a1497a12bd796b9839a27275441577a59
sithusss <74533456+sithusss@users.noreply.github.com>
2021-08-27 10:53:49 +0530

    Update index.js
    add buttons

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 function myfunction(){
-    window.open("index.html");
+    window.open("index.html","_self");
 }
 function apply(){
     window.open("apply.html")
 }
 function about(){
-    window.open("about.html")
+    window.open("about.html","_self")
 }
 function home(){
-    window.open("index.html")
+    window.open("index.html","_self")
 }
+function bio(){
+    window.open("biologin.html","_self")
+}
+function chem(){
+    window.open("chemlogin.html","_self")
+}
+function cs(){
+    window.open("cslogin.html","_self")
+}
+function engi(){
+    window.open("engilogin.html","_self")
+}
+
