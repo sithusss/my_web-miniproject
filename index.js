a9acf372914d0d160448ab86a7e70b83d051dee2
sithusss <74533456+sithusss@users.noreply.github.com>
2021-08-27 12:03:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,24 +2,24 @@ function myfunction(){
     window.open("index.html","_self");
 }
 function apply(){
-    window.open("apply.html")
+    window.open("apply.html");
 }
 function about(){
-    window.open("about.html","_self")
+    window.open("about.html","_self");
 }
 function home(){
-    window.open("index.html","_self")
+    window.open("index.html","_self");
 }
 function bio(){
-    window.open("biologin.html","_self")
+    window.open("biologin.html","_self");
 }
 function chem(){
-    window.open("chemlogin.html","_self")
+    window.open("chemlogin.html","_self");
 }
 function cs(){
-    window.open("cslogin.html","_self")
+    window.open("cslogin.html","_self");
 }
 function engi(){
-    window.open("engilogin.html","_self")
+    window.open("engilogin.html","_self");
 }
 
